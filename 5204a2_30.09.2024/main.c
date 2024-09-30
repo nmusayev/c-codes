39147f771431489fb48b3eff5a51de21d1755f13
Nadir Musayev <nmusayev.dev@gmail.com>
2024-09-30 15:34:44 +0400

    Add files via upload

---
+++ 5204a2_30.09.2024/main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    //     // DATA TYPES in C
+    // // tipi adi = qiymeti;
+    //     char a = 'C';                   // single character    %c
+    //     char b[] = "Baku Engineering University";            // array of characters %s
+    //     // char b2[] = {'B', 'a', 'k', 'u'};
+    //     char b3[] = "Baku";
+    //
+    //     printf("I am in '%s'abcd\n", b3);
+    //
+    //     float c = 3.141592;                          // 4 bytes (32 bits of precision) 6 - 7 digits %f
+    //     double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15 - 16 digits %lf
+    //
+    //     bool e = true;                         // 1 byte (true or false) %d
+    //
+    //     char f = 120;                          // 1 byte (-128 to +127) %d or %c
+    //     unsigned char g = 255;       // 1 byte (0 to +255) %d or %c
+    //
+    //     short h = 32767;                   // 2 bytes (−32,768 to +32,767) %d
+    //     unsigned short i = 65535;  // 2 bytes (0 to +65,535) %d
+    //
+    //     int j = 2147483647;                     // 4 bytes (-2,147,483,648 to +2,147,483,647) %d
+    //     unsigned int k = 4294967295;  // 4 bytes (0 to +4,294,967,295) %u
+    //
+    //     long long int l = 9223372036854775807; // 8 bytes (-9 quintillion to +9 quintillion) %lld
+    //     unsigned long long int m = 18446744073709551615U; // 8 bytes (0 to +18 quintillion) %llu
+    //
+    //    printf("%c\n", a);  // char
+    //     printf("%s\n", b);  // character array
+    //     printf("%f\n", c);  // float
+    //     printf("%lf\n", d); // double
+    //     printf("%d\n", e);  // bool
+    //     printf("%d\n", f);  // char as numeric value
+    //     printf("%d\n", g);  // unsigned char as numeric value
+    //     printf("%d\n", h);  // short
+    //     printf("%d\n", i);  // unsigned short
+    //     printf("%d\n", j);  // int
+    //    printf("%u\n", k);  // unsigned int
+    //     printf("%lld\n", l);  // long long int
+    //     printf("%llu\n", m);  // unsigned long long int
+
+
+
+    // // FORMAT SPECIFIER % = defines and formats a type of data to be displayed
+    //
+    // // %c = character
+    // // %s = string (array of characters)
+    // // %f = float
+    // // %lf = double
+    // // %d = integer
+    //
+    // // %.1 = decimal precision
+    // // %1 = minimum field width
+    // // %- = left align
+    //
+    // float item1 = 5.75;
+    // float item2 = 10.00;
+    // float item3 = 100.99;
+    //
+    // printf("Item 1: $%8.2f\n", item1);
+    // printf("Item 2: $%8.2f\n", item2);
+    // printf("Item 3: $%8.2f\n", item3);
+
+
+
+    // CONSTANTS
+    // constant = fixed value that cannot be altered by the program during its execution
+
+    // const float PI = 3.14159;
+    //
+    // //PI = 420.69; YOU CAN'T CHANGE THIS
+    //
+    // printf("%f", PI);
+
+
+
+        // Arithmetic Operators
+
+          // + (addition)
+          // - (subtraction)
+          // * (multiplication)
+          // / (division)
+          // % (modulus)
+          // ++ increment
+          // -- decrement
+
+          int x = 5;
+          int y = 2;
+
+    // printf("%d\n", --x);
+
+    // printf("%d\n", x++);
+
+    // printf("%d\n", x);
+
+    // x++; // x = x + 1;
+    // x--; // x = x - 1;
+    // printf("%d\n", x);
+
+    // printf("%d%d", x, y);
+
+    // int diff = x - y;
+    // printf("%d\n", diff);
+
+    // printf("%d\n", x * y);
+    // printf("%d\n", x / y);
+
+    // printf("%10.2f\n", 5 / 2.0);
+
+    //       int z = x + y;
+          // int z = x - y;
+          // int z = x * y;
+          // float z = x / (float) y;
+          // int z = x % y;
+           // x++;
+          // y--;
+
+        // 5239 1517 1650 7952
+
+    //      printf("%d \n", x);
+    //      printf("%d \n", y);
+    // printf("%f \n", (5/(float)2));
+
+
+
+
+
+    return 0;
+}
