2434188d3a67792972dcdc5a41a2e2640f9c9e64
Nadir <nmusayev.dev@gmail.com>
2024-10-08 18:23:00 +0400

    8.10.2024 c codes in classroom

---
+++ 5204_8_10_2024/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // for -> eger say mueyyendirse
+    // 1 -> 1000 ededleri print etmek
+
+    // while -> say mueyyen olmayanda
+    // user n -> n
+
+    // for(int i = 0; i < 10; i++) {
+    //     printf("%d\n", i);
+    // }
+
+    // for(int i = 5; i > 0; i = i - 1) {
+    //     printf("%d\n", i);
+    // }
+
+    // positive
+    // int n;
+    // printf("Enter a number: ");
+    // scanf("%d", &n);
+    //
+    // while(n > 0) {
+    //     printf("%d\n", n);
+    //
+    //     n = n - 1;
+    // }
+
+
+    // numune 2
+    //
+    // int a = 1;
+    //
+    // while(a < 17) {
+    //     printf("%d\n", a);
+    //     a++;
+    // }
+
+
+    // // numune 3
+    // int a = 1;
+    //
+    // while(a++) {
+    //     if(a == 13) {
+    //         continue;
+    //     }
+    //
+    //     // if(a != 13) {
+    //     //     printf("%d\n", a);
+    //     // }
+    //
+    //     printf("%d\n", a);
+    //
+    //     if(a == 15) {
+    //         break;
+    //     }
+    // }
+
+    // int a = 1;
+    //
+    // do {
+    //     a++;
+    // } while(a <= 2);
+    //
+    // printf("%d\n", a);
+
+    // printf("Hello, World!\n");
+    return 0;
+}
