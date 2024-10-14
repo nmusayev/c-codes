24a3916859acfd0616fba7a11952c9c30cf8f61f
Nadir <nmusayev.dev@gmail.com>
2024-10-14 17:06:56 +0400

    14 october codes (task main solutions, not complete)

---
+++ 5094_14_10_2024/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // // Task1
+    // int a, b;
+    //
+    // scanf("%d %d", &a, &b);
+    //
+    // printf("%d\n", a + b);
+
+    // // Task 2
+    // char str[100];
+    //
+    // // scanf("%s", str);
+    // // scanf("%[^\n]%*c", str);
+    // fgets(str, 100, stdin);
+    //
+    // printf("Hello, World!\n%s", str);
+
+    // // Task 3
+    // int a, b;
+    // float c, d;
+    //
+    // scanf("%d %d", &a, &b);
+    // scanf("%f %f", &c, &d);
+    //
+    // printf("%d %d\n", a + b, a - b);
+    // printf("%.1f %.1f\n", c + d, c - d);
+
+    // // Task 5
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    //
+    // for (int i = a; i <= b; i++) {
+    //     // deyishen i , her loop-da
+    //     if(i == 1) {
+    //         printf("one\n");
+    //     } else if(i == 2) {
+    //         printf("two\n");
+    //     } else if(i == 3) {
+    //         printf("three\n");
+    //     } else if(i == 4) {
+    //         printf("four\n");
+    //     } else if(i == 5) {
+    //         printf("five\n");
+    //     } else if(i == 6) {
+    //         printf("six\n");
+    //     } else if(i == 7) {
+    //         printf("seven\n");
+    //     } else if(i == 8) {
+    //         printf("eight\n");
+    //     } else if(i == 9) {
+    //         printf("nine\n");
+    //     } else if(i % 2 == 0) {
+    //         printf("even\n");
+    //     } else {
+    //         printf("odd\n");
+    //     }
+    // }
+
+    // // Task 6
+    // int a;
+    // scanf("%d",&a);
+    //
+    // int sum = 0;
+    //
+    // while(a != 0) {
+    //     int lastDigit = a % 10;
+    //     sum += lastDigit;
+    //
+    //     a = a / 10;
+    // }
+    //
+    // printf("%d",sum);
+
+
+    return 0;
+}
---
+++ 5204_14_10_2024/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Task 1
+    // int a, b;
+    //
+    // scanf("%d %d", &a, &b);
+    //
+    // printf("%d\n", a + b);
+
+
+    // // Task 2
+    // char str[100];
+    // // scanf("%s", str);
+    //
+    // // scanf("%[^\n]%*c", &str);
+    // fgets(str, 100, stdin);
+    //
+    // printf("Hello World!\n%s\n", str);
+
+    // // Task 3
+    // int a, b;
+    // float c, d;
+    //
+    // scanf("%d %d", &a, &b);
+    // scanf("%f %f", &c, &d);
+    //
+    // printf("%d %d\n", a + b, a - b);
+    // printf("%.1f %.1f\n", c + d, c - d);
+
+
+    // // Task 5
+    // int a, b;
+    //
+    // scanf("%d %d", &a, &b);
+    //
+    // for (int i = a; i <= b; i++) {
+    //     if(i == 1) {
+    //         printf("one\n");
+    //     } else if(i == 2) {
+    //         printf("two\n");
+    //     } else if(i == 3) {
+    //         printf("three\n");
+    //     } else if(i == 4) {
+    //         printf("four\n");
+    //     } else if(i == 5) {
+    //         printf("five\n");
+    //     } else if(i % 2 == 0) {
+    //         printf("even\n");
+    //     } else {
+    //         printf("odd\n");
+    //     }
+    // }
+
+
+
+    // int a;
+    // scanf("%d",&a);
+    //
+    // int sum = 0;
+    // while(a != 0) {
+    //     int lastDigit = a % 10;
+    //     sum += lastDigit;
+    //
+    //     a = a / 10;
+    // }
+    //
+    // printf("%d", sum);
+
+
+    return 0;
+}
