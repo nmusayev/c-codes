af94a937b55becdf16697ff2664e8a83d186f4a2
Nadir <nmusayev.dev@gmail.com>
2025-01-31 22:34:16 +0400

    lesson: 31 january classroom codes

---
+++ q5164a_pf1_2025/q5164a_31_01_2025/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // // switch = A more efficient alternative to using many "else if" statements
+    // //                 allows a value to be tested for equality against many cases
+    //
+    //    char grade;
+    //
+    //    printf("\nEnter a letter grade: ");
+    //    scanf("%c", &grade);
+    //
+    //    switch(grade){
+    //       case 'A':
+    //          printf("perfect!\n");
+    //          break;
+    //       case 'B':
+    //          printf("You did good!\n");
+    //          break;
+    //       case 'C':
+    //          printf("You did okay!\n");
+    //          break;
+    //       case 'D':
+    //          printf("At least it's not an F!\n");
+    //          break;
+    //       case 'F':
+    //          printf("YOU FAILED!\n");
+    //          break;
+    //       default:
+    //          printf("Please enter only valid grades");
+    //    }
+
+    // // Loop - for, while, do while
+    // for(int i = 1; i <= 10; i++) {
+    //     printf("%d\n", i);
+    // }
+
+
+
+    // // break
+    // for(int i = 1; i <= 10; i++) {
+    //     if(i == 7) {
+    //         break;
+    //     }
+    //
+    //     printf("%d\n", i);
+    // }
+
+
+    // for(int i = 1; i <= 10; i++) {
+    //     if(i == 7) {
+    //         continue;
+    //     }
+    //
+    //     printf("%d\n", i);
+    // }
+
+
+    // int i = 1;
+    //
+    // while (i <= 10) {
+    //     printf("%d\n", i);
+    //
+    //     i++;
+    // }
+
+
+    // int i = 2000;
+    //
+    // while (i >= 1000) {
+    //     printf("%d\n", i);
+    //
+    //     i--;
+    // }
+
+
+    // int i = 1;
+    // do {
+    //     printf("%d\n", i);
+    //     i++;
+    // } while(i <= -10);
+
+
+    return 0;
+}
