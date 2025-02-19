68526916bfd48eb264d62a9a84fc98090eedc67e
Nadir <nmusayev.dev@gmail.com>
2025-02-19 21:00:07 +0400

    19 feb, classroom codes

---
+++ 5204a2_pf2_2025/5204a2_19_2_2025/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // // cout -> printf
+    // // cin -> scanf
+    //
+    // int a, b;
+    //
+    // cin >> a >> b;
+    //
+    // cout << a << " " << b << endl;
+    // std::cout << "Hello, World!" << std::endl;
+    // std::cout << "Hello, World2!";
+
+
+
+    // // Getting name of user and printing it
+    // std::cout << "What is your full name? ";
+    //
+    // std::string name;
+    //
+    // // // this method used only for getting till space
+    // // std::cin >> name;
+    //
+    // // this is the method to take whole line including spaces between words
+    // getline(cin, name);
+    //
+    // std::cout << "Hello, " << name << std::endl;
+
+
+
+    // Operators and Expressions:
+    // Arithmetic + - * / %
+    // Relational < <= > >= == !=
+    // Logical && || !
+    // Bitwise & | ~ ^ << >>
+    // Increment/Decrement ++ --
+    // Assignment =
+
+
+    cout << (3 & 2) << endl;
+    cout << (3 | 2) << endl;
+
+    // more info: https://www.geeksforgeeks.org/cpp-bitwise-operators/
+
+
+
+    return 0;
+}
