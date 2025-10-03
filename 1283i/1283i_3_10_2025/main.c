42eae01df0ade9f8b411c097ddfb54b0956a30df
Nadir <nmusayev.dev@gmail.com>
2025-10-04 02:30:19 +0400

    1283i first lesson

---
+++ 1283i/1283i_3_10_2025/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <_time.h>
+
+/* lkjasdfljkasf
+ * kljasdfjlkj
+ * asfdlkj
+*/
+
+int main(void)
+{
+    // 1 setirlik komment
+    printf("Hello, World!\n");
+
+    printf("Abcd\n");
+    printf("Abcd\n");
+
+    printf("1\t2\t3\t4\n");
+
+    printf("I study in \"BEU\"");
+
+
+
+    return 0;
+}
