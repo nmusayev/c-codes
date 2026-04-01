f20ce68c93f99c76cfafab6394c0d16ef9b65cfe
Nadir Musayev <nmusayev.dev@gmail.com>
2026-04-02 02:08:35 +0400

    31 march codes

---
+++ 165a2_pf2_2026/165a2_31_3_2026/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    // Struct vs Class (Basicly the same style in C++) (keyworkds "struct", "class")
+    // 1. in C struct can only have data, but in C++ it can also have functions
+    // 2. By default, data and function is public in struct, but in class by default they are private, need to define public
+    // Struct: Often used for plain data structures with minimal behavior, resembling C-style structs.
+    // Class: Used for object-oriented programming, encapsulating data and behavior.
+
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
