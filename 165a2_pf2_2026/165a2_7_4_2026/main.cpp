ce9611bb3c2f686667313a7014d3da7489250741
Nadir Musayev <nmusayev.dev@gmail.com>
2026-04-07 11:42:11 +0400

    7 april codes

---
+++ 165a2_pf2_2026/165a2_7_4_2026/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+
+// // OOP_L_7 - Operator Overloading
+// class Complex {
+//     public:
+//     int real;
+//     int img; // imaginary
+//
+//     Complex add(Complex c) {
+//         Complex temp;
+//         temp.real = real + c.real;
+//         temp.img = img + c.img;
+//
+//         return temp;
+//     }
+//
+//     // if this exists, we can remove above add() function
+//     Complex operator+(Complex c) {
+//         Complex temp;
+//         temp.real = real + c.real;
+//         temp.img = img + c.img;
+//
+//         return temp;
+//     }
+// };
+
+
+
+// // OOP_L_8 - Operator Overloading with Friend Function
+// class Complex {
+//     public:
+//     int real;
+//     int img;
+//
+//     friend Complex operator+(const Complex &c1, const Complex &c2);
+// };
+//
+// // if "friend" used, function can access private values. mainly used for that
+// Complex operator+(const Complex &c1, const Complex &c2) {
+//     Complex temp;
+//     temp.real = c1.real + c2.real;
+//     temp.img = c1.img + c2.img;
+//
+//     return temp;
+// }
+
+
+
+// OOP_L_9 - Operator Overloading Insersion
+class Complex {
+    public:
+    int real;
+    int img;
+
+    friend ostream & operator<<(ostream &out, Complex &c);
+};
+
+ostream & operator<<(ostream &out, Complex &c) {
+    out << c.real << " + " << c.img << "i" << endl;
+
+    return out;
+}
+
+
+
+
+
+int main() {
+    // // OOP_L_7 - Operator Overloading
+    // Complex c1;
+    // c1.real = 5;
+    // c1.img = 3;
+    // cout << "c1 Real: " << c1.real << endl;
+    // cout << "c1 Img: " << c1.img << endl;
+    //
+    // Complex c2;
+    // c2.real = 10;
+    // c2.img = 5;
+    // cout << "c2 Real: " << c2.real << endl;
+    // cout << "c2 Img: " << c2.img << endl;
+    //
+    // Complex c3;
+    // // c3 = c1.add(c2);
+    // // c3 = c2.add(c1);
+    // c3 = c1 + c2;
+    //
+    // cout << "c3 Real: " << c3.real << endl;
+    // cout << "c3 Img: " << c3.img << endl;
+
+
+
+    // // OOP_L_8 - Operator Overloading with Friend Function
+    // Complex c1, c2, c3;
+    // c1.real = 5;
+    // c1.img = 3;
+    //
+    // c2.real = 10;
+    // c2.img = 5;
+    //
+    // c3 = c1 + c2;
+    //
+    // cout << c3.real << " + i" << c3.img << endl;
+
+
+
+
+    // OOP_L_9 - Operator Overloading. Insersion
+    Complex c1;
+    c1.real = 10;
+    c1.img = 20;
+
+    cout << c1;
+
+
+
+
+
+
+
+
+
+    // // c3 = c1 + c2;
+    //
+    // cout << c3.real << " + i" << c3.img << endl;
+
+
+
+    return 0;
+}
